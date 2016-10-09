34a92a7b2ee0ea446493bdde6b7df2ecf77ec648
donglongcheng <443795530@qq.com>
2016-10-09 15:24:20 +0800

    增加了几个WebAspect注解的方法

--- src/main/java/com/dcnetworkdisk/web/controller/FileController.java
+++ src/main/java/com/dcnetworkdisk/web/controller/FileController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.dcnetworkdisk.common.DcWebResponse;
 import com.dcnetworkdisk.common.vo.OutputWrapper;
 import com.dcnetworkdisk.file.service.FileService;
 import com.dcnetworkdisk.file.service.ShowFileListOutput;
@@ -25,11 +26,13 @@ public class FileController {
 	@Autowired
 	private FileService fileService;
 	
+	@DcWebResponse
 	@RequestMapping(value="/sfl/{secureToken}/{path}",method=RequestMethod.GET)
 	public @ResponseBody OutputWrapper<ShowFileListOutput> showFileList(@PathVariable String secureToken, @PathVariable String path, @RequestParam(required=false) String orderBy, @RequestParam(required=false) String order, @RequestParam(required=false) String limit){
 		return fileService.getFileList(secureToken, path, orderBy, order, limit);
 	}
 	
+	@DcWebResponse
 	@RequestMapping(value="/sfl_web/{secureToken}/{path}",method=RequestMethod.GET)
 	public ModelAndView showFileList_web(@PathVariable String secureToken, @PathVariable String path, @RequestParam(required=false) String orderBy, @RequestParam(required=false) String order, @RequestParam(required=false) String limit){
 		ModelAndView modelAndView = fileService.getFilelist(secureToken, path, orderBy, order, limit);
@@ -41,11 +44,13 @@ public class FileController {
 	public void uploadFile(){
 	}
 	
+	@DcWebResponse
 	@RequestMapping(value="/upload/{secureToken}/{path}", method=RequestMethod.POST)
 	public @ResponseBody OutputWrapper<UploadFileOutput> uploadFile(@PathVariable String secureToken, @PathVariable String path, @RequestParam MultipartFile file){
 		return fileService.uploadFile(secureToken, path, file);
 	}
 	
+	@DcWebResponse
 	@RequestMapping(value="/download/{secureToken}/{path}/{filename:.+}")
 	public ResponseEntity<byte[]> downloadFile(@PathVariable String secureToken, @PathVariable String path, @PathVariable String filename) throws IOException{
 		return fileService.downloadFile(secureToken, path, filename);
